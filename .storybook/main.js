905e01d28d01f3031f98471a66a153dd74ba6ec9
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-23 23:35:59 +0900

    stories.jsがある場所を設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
