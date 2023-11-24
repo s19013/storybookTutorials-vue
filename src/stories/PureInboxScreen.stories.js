b669bcdf535d34141dc8a0aa0c6af446c3672965
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-24 22:36:29 +0900

    なんか色々作ったよ

---
+++ src/stories/PureInboxScreen.stories.js
@@ -0,0 +1,15 @@
+
+import PureInboxScreen from '@/components/PureInboxScreen.vue';
+
+export default {
+  component: PureInboxScreen,
+  title: 'PureInboxScreen',
+  tags: ['autodocs'],
+};
+
+export const Default = {};
+
+export const Error = {
+  args: { error: true },
+};
+
