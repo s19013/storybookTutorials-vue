07adc586401e33f06bd5e3b7fe6cb1183a4feced
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-24 18:08:34 +0900

    インストールしたアドオンを使えるように設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/vue3-vite",
