7df8111199e15cd9435ed690d8b4f2d9fcd933ac
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-24 22:29:42 +0900

    piniaを使えるように設定

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia';
 
 import './assets/main.css'
+import './index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia()).mount('#app');
